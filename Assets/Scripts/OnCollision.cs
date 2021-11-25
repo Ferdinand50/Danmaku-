666247f0992ec1236691770ba838ac1b8648d907
creative4U <valentin.emelianoff@gmail.com>
2021-11-25 11:14:43 +0100

    player HP

--- Assets/Scripts/OnCollision.cs
+++ Assets/Scripts/OnCollision.cs
@@ -4,19 +4,25 @@ using UnityEngine;
 
 public class OnCollision : MonoBehaviour
 {
+    public int healthPoints ;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        healthPoints = 3;
     }
 
     void OnCollisionEnter2D(Collision2D coll)
     {
         Destroy(coll.gameObject);
+        healthPoints = healthPoints - 1;
     }
     // Update is called once per frame
     void Update()
     {
-        
+        if (healthPoints == 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
