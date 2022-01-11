6fb51686f707b3acf0f4e694ddae59e86122eb02
creative4U <valentin.emelianoff@gmail.com>
2022-01-11 11:36:56 +0100

    singleGun

---
+++ Assets/Scripts/shooting/singleGun.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class singleGun : MonoBehaviour
+{
+
+    public Transform firePointmiddle;
+    public GameObject bulletPrefab;
+
+    //variables for the calculation when to fire
+    public float fireRate = 0.5F;
+    private float nextFire = 0.0F;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Time.time > nextFire) {
+            Shoot();
+            // calulate when to fire
+            nextFire = Time.time + fireRate;
+        }
+    }
+
+    void Shoot ()
+    {
+
+        var bulletMid = Instantiate(bulletPrefab, firePointmiddle.position, firePointmiddle.rotation);
+        Physics2D.IgnoreCollision(bulletMid.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+
+        Physics2D.IgnoreCollision(bulletMid.GetComponent<Collider2D>(), bulletRight.GetComponent<Collider2D>());
+        Physics2D.IgnoreCollision(bulletMid.GetComponent<Collider2D>(), bulletLeft.GetComponent<Collider2D>());
+
+        bulletMid.tag = "EnemyProjectile";
+
+
+    }
+}
