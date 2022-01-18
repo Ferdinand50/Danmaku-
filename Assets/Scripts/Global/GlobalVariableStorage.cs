3974d2efa2298c3f696d7d1fdcd16702e41b57a8
DESKTOP-5DN6SL3\Ferdi <bwferdi@googlemail.com>
2022-01-18 02:41:05 +0100

    bugfix

--- Assets/Scripts/Global/GlobalVariableStorage.cs
+++ Assets/Scripts/Global/GlobalVariableStorage.cs
@@ -10,11 +10,9 @@ public class GlobalVariableStorage : MonoBehaviour
     public static int score;
     public static Vector2 positionPlayer;
     public static int BossHP;
-<<<<<<< HEAD
-    public static int BossMaxHP = 300;
-=======
+
     public static int BossMaxHP = 2000;
->>>>>>> 89e14cf1f0857a7612a1fbe88f9af8e0c6d2646b
+
     // Start is called before the first frame update
     void Start()
     {
